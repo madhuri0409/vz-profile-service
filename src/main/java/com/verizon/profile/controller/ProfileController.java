d5041f9e29fb3fd2702e84611a38adcad007e889
Ganga Madhuri Maddala <gangamadhuri.m@ad.infosys.com>
2020-06-19 01:06:23 -0500

    updated kafka in profile

--- src/main/java/com/verizon/profile/controller/ProfileController.java
+++ src/main/java/com/verizon/profile/controller/ProfileController.java
@@ -12,11 +12,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.verizon.profile.component.CustomerComponent;
 import com.verizon.profile.model.CustomerDetails;
 import com.verizon.profile.model.EquipmentDetails;
 import com.verizon.profile.model.Profile;
+
+import brave.Tracer;
 
 @RestController
 @EnableCircuitBreaker
@@ -32,9 +33,16 @@ public class ProfileController {
 	
 	@Autowired
 	private CustomerComponent customerComponent;
+	
+	@Value("${kafka.service.uri}")
+	private String kafkaServiceUri;
+	
+	@Autowired
+	Tracer tracer;
 
 	@GetMapping("/getProfileById")
 	public Profile getCustomerById(@RequestParam(value = "id") Integer id) {
+		Long currentTime = System.currentTimeMillis();
 		LOGGER.info("In Profile Service : Fetching  Profile Details for Customer Id {}",id);
 		Profile profile = new Profile();
 		CustomerDetails customerDetails = customerComponent.getCustomerDetails(id);
@@ -42,6 +50,10 @@ public class ProfileController {
 				List.class);
 		profile.setCustomerDetails(customerDetails);
 		profile.setEquipmentDetailsList(equipmentDetailsList);
+		
+		String message = String.format("%d | %s | %s | %s | %s | %s", currentTime, "CustomerService", tracer.currentSpan().context().spanIdString(), tracer.currentSpan().context().traceIdString(), id, profile);
+		restTemplate.getForObject(kafkaServiceUri + "=" + message, String.class);
+		
 		return profile;
 	}
 
